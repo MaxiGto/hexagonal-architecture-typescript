e724de928f52e9fae3251ce2cedb21af6a4ece7e
Maxi <maxi.gatto96@gmail.com>
2023-04-22 23:59:58 -0300

    [FT] Dependencias actualizadas

--- src/infrastructure/dependencies.ts
+++ src/infrastructure/dependencies.ts
@@ -1,7 +1,9 @@
 import { WelcomeEmailSender } from "../application/welcome-email-sender";
+import { FakeEmailSender } from "./fake-email-sender";
 import { InMemoryUserRepository } from "./in-memory-user-repository";
 import { UserController } from "./user-controller";
 
+const fakeEmailSender = new FakeEmailSender();
 const inMemoryUserRepository = new InMemoryUserRepository();
-export const welcomeEmailSender = new WelcomeEmailSender(inMemoryUserRepository);
+ const welcomeEmailSender = new WelcomeEmailSender(inMemoryUserRepository, fakeEmailSender);
 export const userController = new UserController(welcomeEmailSender);
