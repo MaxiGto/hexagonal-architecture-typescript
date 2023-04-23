4668de16038645cefefce629a119543114a910f4
Maxi <maxi.gatto96@gmail.com>
2023-04-22 23:59:46 -0300

    [FT] Welcome email sender actualizado

--- src/application/welcome-email-sender.ts
+++ src/application/welcome-email-sender.ts
@@ -1,8 +1,9 @@
+import { EmailSender } from "../domain/email-sender";
 import { UserRepository } from "../domain/user-repository";
 
 export class WelcomeEmailSender {
 
-    constructor(private readonly userRepository: UserRepository){}
+    constructor(private readonly userRepository: UserRepository, private readonly emailSender: EmailSender){}
 
     async run(userId: string) {
         console.log("User", userId);
@@ -11,6 +12,8 @@ export class WelcomeEmailSender {
         if(!user) throw new Error(`User id not found ${userId}`);
 
         console.log("User", user.email);
+
+        await this.emailSender.send(user.email, "Welcome to our application!");
     }
 
 }
