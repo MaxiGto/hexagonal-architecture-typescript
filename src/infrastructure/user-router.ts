5270246e2be0978dc2134cf6f22a527fe3efe9fa
Maxi <maxi.gatto96@gmail.com>
2023-04-22 23:17:17 -0300

    [FT] User controller and router

---
+++ src/infrastructure/user-router.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { userController } from "./dependencies";
+
+const userRouter = express.Router();
+
+userRouter.post("/:id/welcome", userController.run.bind(userController));
+
+export { userRouter };
